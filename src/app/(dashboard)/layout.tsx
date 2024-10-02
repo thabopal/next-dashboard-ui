92efa305134adfe3a16cc4861402a6d734f4e0f6
Thabo <tpali@cut.ac.za>
2024-10-02 21:34:22 +0200

    Made significant changes on the dashboard

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import Image from "next/image";
+import Menu from "../components/Menu";
+import Navbar from "../components/Navbar";
+
+export default function DashboardLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (
+        <div className="h-screen flex">
+          {/* <!-- LEFT Sidebar --> */}
+          <div className="w-[14%] md:w[8%] lg:w-[16%] xl:w[16%] p-4">
+            <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
+              <Image src="/logo.png" alt="logo" width={32} height={32}/>
+              <span className="hidden lg:block">Academy360</span>
+            </Link>
+            <Menu/>
+          </div>
+          {/* <!-- RIGHT Sidebar --> */}
+          <div className="w-[86%] md:w[92%] lg:w-[84%] xl:w-[84%] bg-[#F7F8FA] overflow-scroll">
+            <Navbar/>
+            {children}
+          </div>
+        </div>
+    );
+  }
