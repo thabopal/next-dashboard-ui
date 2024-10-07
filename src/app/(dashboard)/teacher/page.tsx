e26a0ad72f522e456aafffffad79f6b9e03f2eb8
Thabo <tpali@cut.ac.za>
2024-10-07 15:10:01 +0200

    Update teacher and parent pages

--- src/app/(dashboard)/teacher/page.tsx
+++ src/app/(dashboard)/teacher/page.tsx
@@ -1,7 +1,21 @@
+import Announcements from "@/app/components/Announcements";
+import BigCalendar from "@/app/components/BigCalendar";
+import EventCalendar from "@/app/components/EventCalendar";
+
 const TeacherPage = () => {
     return (
-        <div className="">
-            Teacher Page
+        <div className="flex-1 p-4 flex gap-4 flex-col xl:flex-row">
+            {/* LEFT */}
+            <div className="w-full xl:W-2/3">
+                <div className="h-full bg-white p-4 rounded-md">
+                    <h1 className="text-xl font-semibold">Timetable</h1>
+                    <BigCalendar />
+                </div>
+            </div>
+            {/* RIGHT */}
+            <div className="w-full xl:w-1/3 flex flex-col gap-8">
+                <Announcements />
+            </div>
         </div>
     );
 };
