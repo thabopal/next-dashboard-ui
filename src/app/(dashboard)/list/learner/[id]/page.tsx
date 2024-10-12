8e72137b827f623aebc6e88adaf6680699885a67
Thabo <tpali@cut.ac.za>
2024-10-12 21:32:15 +0200

    View learner or teacher by id UI works

--- src/app/(dashboard)/list/learner/[id]/page.tsx
+++ src/app/(dashboard)/list/learner/[id]/page.tsx
@@ -4,7 +4,7 @@ import Performance from "@/app/components/Performance"
 import Image from "next/image"
 import Link from "next/link"
 
-const SingleTeacherPage = () => {
+const SingleLearnerPage = () => {
     return (
         <div className="flex-1 p-4 flex flex-col gap-4 xl:flex-row">
             {/* LEFT */}
@@ -14,10 +14,10 @@ const SingleTeacherPage = () => {
                     {/* USER INFO CARD */}
                     <div className="bg-lamaSky py-6 px-4 rounded-md flex-1 flex gap-4">
                         <div className="w-1/3">
-                            <Image src="https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=1200" alt="" width={144} height={144} className="w-36 h-36 rounded-full object-cover" />
+                            <Image  src="https://images.pexels.com/photos/5414817/pexels-photo-5414817.jpeg?auto=compress&cs=tinysrgb&w=1200" alt="" width={144} height={144} className="w-36 h-36 rounded-full object-cover" />
                         </div>
                         <div className="w-2/3 flex flex-col justify-between gap-4">
-                            <h1 className="text-xl font-semibold">Thabo Pali</h1>
+                            <h1 className="text-xl font-semibold">Lesedi Mabina</h1>
                             <p className="text-sm text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                             <div className="flex items-center justify-between gap-2 flex-wrap text-xs font-medium">
                                 <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
@@ -54,14 +54,14 @@ const SingleTeacherPage = () => {
                             <Image src="/singleBranch.png" alt="" width={24} height={24} className="w-6 h-6"/>
                             <div className="text-xl font-semibold">
                                 <h1>6</h1>
-                                <span className="text-sm text-gray-400">Branches</span>
+                                <span className="text-sm text-gray-400">Grade</span>
                             </div>
                         </div>
                         {/* CARD */}
                         <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:[48%] 2xl[48%] 2xl:w-[48%]">
                             <Image src="/singleLesson.png" alt="" width={24} height={24} className="w-6 h-6"/>
                             <div className="text-xl font-semibold">
-                                <h1>6</h1>
+                                <h1>16</h1>
                                 <span className="text-sm text-gray-400">Lessons</span>
                             </div>
                         </div>
@@ -69,8 +69,8 @@ const SingleTeacherPage = () => {
                         <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:[48%] 2xl[48%] 2xl:w-[48%]">
                             <Image src="/singleClass.png" alt="" width={24} height={24} className="w-6 h-6"/>
                             <div className="text-xl font-semibold">
-                                <h1>6</h1>
-                                <span className="text-sm text-gray-400">Classes</span>
+                                <h1>6A</h1>
+                                <span className="text-sm text-gray-400">Class</span>
                             </div>
                         </div>
                     </div>
@@ -86,11 +86,11 @@ const SingleTeacherPage = () => {
                 <div className="bg-white p-4 rounded-md">
                     <h1 className="text-xl font-semibold">Shortcuts</h1>
                     <div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500">
-                        <Link className="p-3 rounded-md bg-lamaSkyLight" href="/">Teacher&apos;s Classes</Link>
-                        <Link className="p-3 rounded-md bg-lamaPurpleLight" href="/">Teacher&apos;s Students</Link>
-                        <Link className="p-3 rounded-md bg-lamaYellowLight" href="/">Teacher&apos;s Lessons</Link>
-                        <Link className="p-3 rounded-md bg-pink-50" href="/">Teacher&apos;s Exams</Link>
-                        <Link className="p-3 rounded-md bg-lamaSkyLight" href="/">Teacher&apos;s Assignments</Link>
+                        <Link className="p-3 rounded-md bg-lamaSkyLight" href="/">Learner&apos;s Lessons</Link>
+                        <Link className="p-3 rounded-md bg-lamaPurpleLight" href="/">Learner&apos;s Teachers</Link>
+                        <Link className="p-3 rounded-md bg-lamaYellowLight" href="/">Learner&apos;s Results</Link>
+                        <Link className="p-3 rounded-md bg-pink-50" href="/">Learner&apos;s Exams</Link>
+                        <Link className="p-3 rounded-md bg-lamaSkyLight" href="/">Learner&apos;s Assignments</Link>
                     </div>
                 </div>
                 <Performance />
@@ -100,4 +100,4 @@ const SingleTeacherPage = () => {
     )
 }
 
-export default SingleTeacherPage
+export default SingleLearnerPage
