fdcd690081100e97cbb8916eee8d5953c7b84014
Thabo <tpali@cut.ac.za>
2024-11-08 13:45:07 +0200

    Show logged in users first and lastname

--- src/app/components/BigCalendar.tsx
+++ src/app/components/BigCalendar.tsx
@@ -2,7 +2,6 @@
 
 import { Calendar, momentLocalizer, View, Views } from 'react-big-calendar'
 import moment from 'moment'
-import { calendarEvents } from '@/lib/data'
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { useState } from 'react';
 
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -23,7 +23,7 @@ const Navbar = async () => {
                 <div className="absolute -top-3 -right-3 w-5 h-5 flex items-center justify-center bg-purple-500 text-white rounded-full text-xs">1</div>
             </div>
             <div className="flex flex-col">
-                <span className='text-xs leading-3 font-medium'>Thabo Pali</span>
+                <span className='text-xs leading-3 font-medium'>{user?.firstName + " " + user?.lastName}</span>
                 <span className='text-[10px] text-gray-500 text-right'>{user?.publicMetadata.role as string}</span>
             </div>
             {/* <Image src="/avatar.png" alt="" width={36} height={36} className='rounded-full' /> */}
